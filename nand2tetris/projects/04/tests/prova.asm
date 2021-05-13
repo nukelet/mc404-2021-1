9716ba4931324c0187d9494b895682aff29facbb
Vinícius Peixoto <vinicius241201@gmail.com>
2021-05-13 10:28:11 -0300

    Add .asm for problem in Exam 1

---
+++ nand2tetris/projects/04/tests/prova.asm
@@ -0,0 +1,55 @@
+@6
+D=A
+@R0
+M=D
+
+@4
+D=A
+@R1
+M=D
+
+@3
+D=A
+@R2
+M=D
+
+@R0
+D=M
+@R1
+D=D+M
+@R2
+D=D-M
+@NOT_TRIANGLE
+D;JLE
+
+@R1
+D=M
+@R2
+D=D+M
+@R0
+D=D-M
+@NOT_TRIANGLE
+D;JLE
+
+@R2
+D=M
+@R0
+D=D+M
+@R1
+D=D-M
+@NOT_TRIANGLE
+D;JLE
+
+@R4
+M=1
+
+@END
+0;JMP
+
+(NOT_TRIANGLE)
+    @R4
+    M=0
+
+(END)
+    @END
+    0;JMP
